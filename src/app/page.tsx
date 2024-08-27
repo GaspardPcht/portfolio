cb3ce63bdf3d072ed9424a68b87ebf91ecafe0d0
Gaspard <Your email>
2024-08-27 15:04:38 +0200

    change button start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function StartScreen() {
   return (
-    <div className="flex flex-col h-screen p-1 box-border relative ">
+    <div className="flex flex-col h-screen p-1 box-border relative">
       <header className="flex items-center justify-between ml-8 mb-16 p-5">
         <span className="text-2xl font-bold font-[League Spartan] text-black">
           GASPARDPCHT
@@ -52,7 +52,7 @@ export default function StartScreen() {
       </div>
       <div className="flex justify-end mr-[200px] mb-[100px] mt-5">
         <Link
-          className="w-[200px] inline-block py-2 px-8 text-2xl font-bold text-white bg-black rounded-lg text-center no-underline transition-colors hover:bg-gray-800"
+          className="w-[200px] inline-block py-2 px-8 text-2xl font-bold text-white bg-black rounded-lg text-center no-underline transition-colors duration-300 hover:bg-white hover:text-black"
           href="/home"
         >
           START
