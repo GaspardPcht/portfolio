c6a86eb50992182e06290379bc1650b7519bc2e0
Gaspard <Your email>
2024-08-28 16:10:48 +0200

    bento

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,14 +1,22 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { Dispatch, SetStateAction, useState } from "react";
-import { FiAlertCircle } from "react-icons/fi";
+import { Dispatch, SetStateAction } from "react";
+import { FaArrowLeft } from "react-icons/fa";
+
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  title?: string;
+  text?: string;
+  link?: string; // Optional link prop
+}
 
 const Modal = ({
   isOpen,
   setIsOpen,
-}: {
-  isOpen: boolean;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
-}) => {
+  title = "Default Title",
+  text = "Default text",
+  link,
+}: ModalProps) => {
   return (
     <AnimatePresence>
       {isOpen && (
@@ -24,35 +32,27 @@ const Modal = ({
             animate={{ scale: 1, rotate: "0deg" }}
             exit={{ scale: 0, rotate: "0deg" }}
             onClick={(e) => e.stopPropagation()}
-            className="bg-gradient-to-br from-violet-600 to-indigo-600 text-white p-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative overflow-hidden"
+            className="bg-[#3C3C3C] text-white p-6 rounded-lg w-full max-w-lg shadow-xl cursor-default relative overflow-hidden"
           >
-            <FiAlertCircle className="text-white/10 rotate-12 text-[250px] absolute z-0 -top-24 -left-24" />
-            <div className="relative z-10">
-              <div className="bg-white w-16 h-16 mb-2 rounded-full text-3xl text-indigo-600 grid place-items-center mx-auto">
-                <FiAlertCircle />
-              </div>
-              <h3 className="text-3xl font-bold text-center mb-2">
-                One more thing!
-              </h3>
-              <p className="text-center mb-6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-                aperiam vitae, sapiente ducimus eveniet in velit.
-              </p>
-              <div className="flex gap-2">
-                <button
-                  onClick={() => setIsOpen(false)}
-                  className="bg-transparent hover:bg-white/10 transition-colors text-white font-semibold w-full py-2 rounded"
-                >
-                  Nah, go back
-                </button>
-                <button
-                  onClick={() => setIsOpen(false)}
-                  className="bg-white hover:opacity-90 transition-opacity text-indigo-600 font-semibold w-full py-2 rounded"
-                >
-                  Understood!
-                </button>
-              </div>
+            <div className="flex gap-2 items-center mb-4">
+              <FaArrowLeft
+                onClick={() => setIsOpen(false)}
+                className="text-white text-xl hover:text-gray-400 cursor-pointer transition-colors"
+              />
             </div>
+
+            <h3 className="text-3xl font-bold text-center mb-2">{title}</h3>
+            <p className="text-center mb-6">{text}</p>
+            {link && (
+              <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block text-center text-blue-500 underline mt-4"
+              >
+                Visit Project
+              </a>
+            )}
           </motion.div>
         </motion.div>
       )}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,8 +5,14 @@ import { useState } from "react";
 
 export default function Projects() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    title: "",
+    text: "",
+    link: "",
+  });
 
-  const handleImageClick = () => {
+  const handleImageClick = (title: string, text: string, link: string) => {
+    setModalContent({ title, text, link });
     setIsModalOpen(true);
   };
 
@@ -18,7 +24,7 @@ export default function Projects() {
         transition={{ delay: 0.5, duration: 0.5 }}
         className="flex flex-col items-start text-[#3C3C3C] mt-[100px] mb-[100px] ml-[60%]"
       >
-        <h1 className="text-4xl font-bold">PROJETS</h1>
+        <h1 className="text-4xl font-bold">PROJECTS</h1>
       </motion.div>
 
       <section>
@@ -26,7 +32,13 @@ export default function Projects() {
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4 h-full">
             <div className="col-span-2 sm:col-span-1 md:col-span-5 flex justify-center items-center">
               <button
-                onClick={handleImageClick}
+                onClick={() =>
+                  handleImageClick(
+                    "Flow Studio",
+                    "Flow Studio is a website dedicated to pole dancing, developed on WordPress. It showcases classes, schedules, and special events, all within an elegant and user-friendly interface.",
+                    "https://poledancewithme.wordpress.com/"
+                  )
+                }
                 className="group relative flex flex-col overflow-hidden rounded-lg bg-[#E3E0DC] w-full h-[100px] p-0 m-0 border-none bg-transparent"
               >
                 <div className="relative w-full h-full">
@@ -44,7 +56,13 @@ export default function Projects() {
             {/* Second Item */}
             <div className="col-span-2 sm:col-span-1 md:col-span-2 h-auto md:h-full flex flex-col">
               <button
-                onClick={handleImageClick}
+                onClick={() =>
+                  handleImageClick(
+                    "Dog Around",
+                    "Dog Around is a social network for dog lovers, allowing users to connect with each other and share their experiences.",
+                    "https://dogaround.com/"
+                  )
+                }
                 className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow border-none bg-transparent"
               >
                 <Image
@@ -60,38 +78,56 @@ export default function Projects() {
             {/* Third Item */}
             <div className="col-span-2 sm:col-span-1 md:col-span-2 h-auto md:h-full flex flex-col">
               <button
-                onClick={handleImageClick}
+                onClick={() =>
+                  handleImageClick(
+                    "Cinema Hub",
+                    "Cinema Hub is a movie review platform, offering detailed reviews and ratings for the latest films.",
+                    "https://cinemahub.com/"
+                  )
+                }
                 className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 mb-4 border-none bg-transparent"
               >
                 <Image
                   fill
                   src="/assets/image/cinema.avif"
-                  alt="Cinema"
+                  alt="Cinema Hub"
                   className="absolute inset-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                 />
                 <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
               </button>
               <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
                 <button
-                  onClick={handleImageClick}
+                  onClick={() =>
+                    handleImageClick(
+                      "Train Timetable",
+                      "Train Timetable is a travel app providing up-to-date information on train schedules and routes.",
+                      "https://traintimetable.com/"
+                    )
+                  }
                   className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 border-none bg-transparent"
                 >
                   <Image
                     fill
                     src="/assets/image/train.avif"
-                    alt="Train"
+                    alt="Train Timetable"
                     className="absolute inset-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                   />
                   <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
                 </button>
                 <button
-                  onClick={handleImageClick}
+                  onClick={() =>
+                    handleImageClick(
+                      "Twitter Trends",
+                      "Twitter Trends is a social media dashboard that tracks trending topics and hashtags on Twitter.",
+                      "https://twittertrends.com/"
+                    )
+                  }
                   className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 border-none bg-transparent"
                 >
                   <Image
                     fill
                     src="/assets/image/twitter.avif"
-                    alt="Twitter"
+                    alt="Twitter Trends"
                     className="absolute inset-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                   />
                   <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
@@ -102,13 +138,19 @@ export default function Projects() {
             {/* Fourth Item */}
             <div className="col-span-2 sm:col-span-1 md:col-span-1 h-auto md:h-full flex flex-col">
               <button
-                onClick={handleImageClick}
+                onClick={() =>
+                  handleImageClick(
+                    "Weather Forecast",
+                    "Weather Forecast is an app that provides real-time weather updates and forecasts for cities around the world.",
+                    "https://weatherforecast.com/"
+                  )
+                }
                 className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow border-none bg-transparent"
               >
                 <Image
                   fill
                   src="/assets/image/weather.avif"
-                  alt="Weather"
+                  alt="Weather Forecast"
                   className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                 />
                 <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
@@ -118,14 +160,20 @@ export default function Projects() {
             {/* Fifth Item */}
             <div className="col-span-2 sm:col-span-1 md:col-span-5 flex justify-center items-center">
               <button
-                onClick={handleImageClick}
+                onClick={() =>
+                  handleImageClick(
+                    "New York Times Archive",
+                    "The New York Times Archive offers access to historical articles and publications from the iconic newspaper.",
+                    "https://nytimesarchive.com/"
+                  )
+                }
                 className="group relative flex flex-col overflow-hidden rounded-lg bg-[#E3E0DC] w-full h-[100px] p-0 m-0 border-none bg-transparent"
               >
                 <div className="relative w-full h-full">
                   <Image
                     fill
                     src="/assets/image/newYorkTimes.avif"
-                    alt="New York Times"
+                    alt="New York Times Archive"
                     className="absolute inset-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                   />
                   <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
@@ -136,8 +184,13 @@ export default function Projects() {
         </div>
       </section>
 
-      {/* Modal Component */}
-      <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
+      <Modal
+        isOpen={isModalOpen}
+        setIsOpen={setIsModalOpen}
+        title={modalContent.title}
+        text={modalContent.text}
+        link={modalContent.link}
+      />
     </div>
   );
 }
