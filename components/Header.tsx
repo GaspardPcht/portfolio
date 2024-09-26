85165b70dfb6fea52a2b81646d504a10627feb65
Gaspard <Your email>
2024-09-26 16:11:54 +0200

    chore: Update Header component to include responsive menu and social media links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,8 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import BubbleText from "./BubbleText";
-import { useState } from "react";
 
 interface HeaderProps {
   readonly scrollToAbout: () => void;
@@ -20,7 +19,7 @@ export default function Header({
 
   return (
     <div className="relative flex items-center justify-between ml-8 p-6 border-b border-gray-300">
-      <div className="flex items-center w-full gap-[80px] text-black">
+      <div className="flex items-center w-full  justify-between  text-black">
         <motion.span
           className="text-xl font-bold font-[League Spartan] text-black stroke-black stroke-1 relative z-10"
           initial={{ opacity: 0, y: 20 }}
@@ -30,71 +29,66 @@ export default function Header({
           <BubbleText text="GASPARDPCHT" />
         </motion.span>
         <button
-          className="md:hidden hover:bg-transparent text-3xl text-black ml-[160px] relative z-20"
+          className="md:hidden hover:bg-transparent text-3xl text-black relative z-20"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           aria-label="Toggle Menu"
         >
           &#9776;
         </button>
         <div className="gap-[100px] hidden md:flex font-semibold text-[#3C3C3C]">
-          <motion.span
-            onClick={scrollToAbout}
-            className="cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#3C3C3C] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            About
-          </motion.span>
-          <motion.span
-            onClick={scrollToProject}
-            className="cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#3C3C3C] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-          >
-            Projects
-          </motion.span>
-          <motion.span
-            onClick={scrollToContact}
-            className="cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#3C3C3C] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.4 }}
-          >
-            Contact
-          </motion.span>
+          {["About", "Projects", "Contact"].map((item, index) => (
+            <motion.span
+              key={item}
+              onClick={
+                item === "About"
+                  ? scrollToAbout
+                  : item === "Projects"
+                  ? scrollToProject
+                  : scrollToContact
+              }
+              className="cursor-pointer relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#3C3C3C] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.2 }}
+            >
+              {item}
+            </motion.span>
+          ))}
         </div>
       </div>
       <div className="hidden md:flex gap-[30px] items-center mr-5">
-        <motion.a
-          href="https://www.linkedin.com/in/gaspard-pauchet-348044215/"
-          target="_blank"
-          rel="noopener noreferrer"
-          whileHover={{ scale: 1.1, rotate: 2 }}
-          transition={{ duration: 0.3 }}
-        >
-          <Image
-            src="/assets/icons/logo-linkedin-noir.png"
-            alt="Logo LinkedIn"
-            width={50}
-            height={50}
-          />
-        </motion.a>
-        <motion.a
-          href="https://github.com/GaspardPcht"
-          target="_blank"
-          rel="noopener noreferrer"
-          whileHover={{ scale: 1.1, rotate: 2 }}
-          transition={{ duration: 0.3 }}
-        >
-          <Image
-            src="/assets/icons/github.svg"
-            alt="Logo GitHub"
-            width={35}
-            height={35}
-          />
-        </motion.a>
+        {[
+          {
+            href: "https://www.linkedin.com/in/gaspard-pauchet-348044215/",
+            src: "/assets/icons/logo-linkedin-noir.png",
+            alt: "Logo LinkedIn",
+            width: 50,
+            height: 50,
+          },
+          {
+            href: "https://github.com/GaspardPcht",
+            src: "/assets/icons/github.svg",
+            alt: "Logo GitHub",
+            width: 35,
+            height: 35,
+          },
+        ].map((link) => (
+          <motion.a
+            key={link.href}
+            href={link.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            whileHover={{ scale: 1.1, rotate: 2 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Image
+              src={link.src}
+              alt={link.alt}
+              width={link.width}
+              height={link.height}
+            />
+          </motion.a>
+        ))}
       </div>
       <div
         className={`fixed top-0 right-0 w-3/4 max-w-xs bg-white h-full border-l border-gray-300 transition-transform transform ${
@@ -109,62 +103,55 @@ export default function Header({
           &times;
         </button>
         <div className="flex flex-col items-center mt-20">
-          <motion.span
-            onClick={() => {
-              scrollToAbout();
-              setIsMenuOpen(false);
-            }}
-            className="my-4 text-xl font-semibold cursor-pointer"
-          >
-            About
-          </motion.span>
-          <motion.span
-            onClick={() => {
-              scrollToProject();
-              setIsMenuOpen(false);
-            }}
-            className="my-4 text-xl font-semibold cursor-pointer"
-          >
-            Projects
-          </motion.span>
-          <motion.span
-            onClick={() => {
-              scrollToContact();
-              setIsMenuOpen(false);
-            }}
-            className="my-4 text-xl font-semibold cursor-pointer"
-          >
-            Contact
-          </motion.span>
+          {["About", "Projects", "Contact"].map((item) => (
+            <motion.span
+              key={item}
+              onClick={() => {
+                item === "About"
+                  ? scrollToAbout()
+                  : item === "Projects"
+                  ? scrollToProject()
+                  : scrollToContact();
+                setIsMenuOpen(false);
+              }}
+              className="my-4 text-xl font-semibold cursor-pointer"
+            >
+              {item}
+            </motion.span>
+          ))}
           <div className="flex gap-4 my-4">
-            <motion.a
-              href="https://www.linkedin.com/in/gaspard-pauchet-348044215/"
-              target="_blank"
-              rel="noopener noreferrer"
-              whileHover={{ scale: 1.1, rotate: 2 }}
-              transition={{ duration: 0.3 }}
-            >
-              <Image
-                src="/assets/icons/logo-linkedin-noir.png"
-                alt="Logo LinkedIn"
-                width={30}
-                height={30}
-              />
-            </motion.a>
-            <motion.a
-              href="https://github.com/GaspardPcht"
-              target="_blank"
-              rel="noopener noreferrer"
-              whileHover={{ scale: 1.1, rotate: 2 }}
-              transition={{ duration: 0.3 }}
-            >
-              <Image
-                src="/assets/icons/github.svg"
-                alt="Logo GitHub"
-                width={30}
-                height={30}
-              />
-            </motion.a>
+            {[
+              {
+                href: "https://www.linkedin.com/in/gaspard-pauchet-348044215/",
+                src: "/assets/icons/logo-linkedin-noir.png",
+                alt: "Logo LinkedIn",
+                width: 30,
+                height: 30,
+              },
+              {
+                href: "https://github.com/GaspardPcht",
+                src: "/assets/icons/github.svg",
+                alt: "Logo GitHub",
+                width: 30,
+                height: 30,
+              },
+            ].map((link) => (
+              <motion.a
+                key={link.href}
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ scale: 1.1, rotate: 2 }}
+                transition={{ duration: 0.3 }}
+              >
+                <Image
+                  src={link.src}
+                  alt={link.alt}
+                  width={link.width}
+                  height={link.height}
+                />
+              </motion.a>
+            ))}
           </div>
         </div>
       </div>
